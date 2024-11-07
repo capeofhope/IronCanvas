046d6cd7a06511f96d8fc349e4a3f2546c648e7c
Mrinal <mrinalkandwal@gmail.com>
2024-11-08 02:06:15 +0530

    feat: add useApiMutation hook and define schema for boards
    - Implemented `useApiMutation` hook to streamline API mutations for board-related operations.
    - Defined schema for boards to validate data structure and types.
    - Enhanced code organization and maintainability for future API interactions.

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useOrganization } from "@clerk/clerk-react";
 import Image from "next/image";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+  const onClick = () => {
+    if (!organization) return;
+    mutate({ title: "Untitled", orgId: organization?.id })
+      .then((id) => {
+        toast.success("Board created");
+        //TODO: Redirect to board(id)
+      })
+      .catch(() => {
+        toast.error("Failed to create board");
+      });
+  };
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src={"/note.svg"} height={150} width={150} alt="Empty" />
@@ -11,7 +28,7 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization.
       </p>
       <div className="mt-6">
-        <Button size={"lg"}>
+        <Button disabled={pending} size={"lg"} onClick={onClick}>
           Create board
         </Button>
       </div>
