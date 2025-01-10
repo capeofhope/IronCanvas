4a75160c5afde748232d4347b5fa87c519dbb364
Mrinal <mrinalkandwal@gmail.com>
2025-01-10 10:05:50 +0530

    feat: implement board list component with API integration and loading states

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,12 @@
 "use client";
-
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 import { EmptySearch } from "./empty-search";
 import { EmptyFavorites } from "./empty-favorites";
 import { EmptyBoards } from "./empty-boards";
+import React from "react";
+import BoardCard from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -13,21 +17,57 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = []; //Todo: Change an API call
-  if (!data?.length && query.search) {
+  //Todo: Change an API call
+  const data = useQuery(api.boards.get, { orgId });
+  // console.log(data);
+  if (data===undefined) {
     return (
-      <EmptySearch/>
+      <div className="">
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite boards" : "Team boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
     );
+  }
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
   }
   if (!data?.length && query.favorites) {
-    return (
-      <EmptyFavorites/>
-    );
+    return <EmptyFavorites />;
   }
   if (!data?.length) {
-    return (
-      <EmptyBoards/>
-    );
+    return <EmptyBoards />;
   }
-  return <div className="">{JSON.stringify(query)}</div>;
+  return (
+    <div className="">
+      <h2 className="text-3xl">
+        {query.favorites ? "Favorite boards" : "Team boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
---
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { toast } from "sonner";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { cn } from "@/lib/utils";
+import { Plus } from "lucide-react";
+
+interface NewBoardButtonProps {
+  orgId: string;
+  disabled?: boolean;
+}
+
+export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+  const onClick = () => {
+    mutate({
+      orgId,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+        //todo: redirect to the new board /board/${id}
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+  return (
+    <button
+      disabled={pending || disabled}
+      onClick={onClick}
+      className={cn(
+        "col-span-1 aspect-[100/127] bg-blue-600 border rounded-lg flex flex-col items-center justify-center py-6",
+        (pending || disabled) &&
+          "hover:bg-blue-600 opacity-75 cursor-not-allowed"
+      )}
+    >
+      <div className=""></div>
+      <Plus className="h-12 w-12 text-white stroke-1" />
+      <p className="text-sm text-white font-light">New board</p>
+    </button>
+  );
+};
