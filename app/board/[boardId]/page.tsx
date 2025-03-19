40f32da82abc26225b62788d40c15c2f7d9852f2
Mrinal <mrinalkandwal@gmail.com>
2025-03-19 22:04:16 +0530

    feat: add loading skeletons for info, participants, and toolbar components; implement Room component for real-time collaboration using liveblocks

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,14 +1,18 @@
+import { Room } from "@/components/room";
 import { Canvas } from "./_components/canvas";
+import { Loading } from "./_components/loading";
 interface BoardIdPageProps {
   params: {
     boardId: string;
   };
-
 }
-const BoardIdPage = ({params}:BoardIdPageProps) => {
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { boardId } = await params;
   return (
     <>
-      <Canvas boardId={params.boardId}/>
+      <Room roomId={boardId} fallback={<Loading/>}>
+        <Canvas boardId={boardId} />
+      </Room>
     </>
   );
 };
---
+++ components/room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { ClientSideSuspense } from "@liveblocks/react/suspense";
+import { RoomProvider } from "../liveblocks.config";
+
+interface RoomProps {
+  children: ReactNode;
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
+}
+export function Room({ children, roomId,fallback }: RoomProps) {
+  return (
+    <RoomProvider id={roomId} initialPresence={{}}>
+      <ClientSideSuspense fallback={fallback}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
